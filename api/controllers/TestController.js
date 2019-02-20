65367b08453525091822359f9e6e8222f0ef606b
Rachael Shaw <r@rachael.wtf>
2019-02-20 09:17:57 -0600

    Add missing `await`

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -2,7 +2,9 @@
 module.exports = {
   
   test: async (req, res) => {
-    const created = await sails.getDatastore().transaction(db => Model.create({ first: 'test', last: 'test' }).usingConnection(db).fetch());
+    const created = await sails.getDatastore().transaction(async (db) => {
+      await Model.create({ first: 'test', last: 'test' }).usingConnection(db).fetch();
+    });
 
     return res.json(created);
   }
